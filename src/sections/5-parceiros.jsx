1a12099d838e9fd0129414c8c776e031424f6994
Eduardo Borges <euduardoborges@gmail.com>
2019-05-30 10:32:46 -0300

    Parceiros

--- src/sections/5-parceiros.jsx
+++ src/sections/5-parceiros.jsx
@@ -2,12 +2,32 @@ import React from "react";
 
 import { palestrantes } from "../data";
 
-const colors = {
-  web: 'is-danger',
-  ia: 'is-warning',
-  android: 'is-success',
-  iot: 'is-info'
-};
+const parceiros = [
+  {
+    image: "http://sergipetec.org.br/wp-content/uploads/2016/04/sergipetec300x140.png",
+    url: "http://sergipetec.org.br/"
+  },
+  {
+    image: "https://vestibular.unit.br/se/wp-content/uploads/2018/04/logo-1.png",
+    url: "http://unit.br"
+  },
+  {
+    image: "http://processoseletivo.fanese.edu.br/assets/images/logo-fanese-126x134.png",
+    url: "http://fanese.edu.br"
+  },
+  {
+    image: "http://portal.estacio.br/img/logo-estacio-horizontal.png",
+    url: "http://portal.estacio.br/"
+  },
+  {
+    image: "https://www.uninassau.edu.br/landpages/nassau/img/layout/logo-small.png",
+    url: "https://www.uninassau.edu.br/"
+  },
+  {
+    image: "https://git.dcomp.ufs.br/uploads/-/system/appearance/logo/1/LOGOTIPO_ORIGINAL_RETANGULAR.png",
+    url: "http://computacao.ufs.br"
+  }
+];
 
 function Parceiros() {
   return (
@@ -24,10 +44,10 @@ function Parceiros() {
 
           {/* items */}
           <div className="columns is-centered is-multiline">
-            {[1,2,3,4,5,6,7,8].map(pessoa => (
-              <div className="column is-3 has-text-centered">
-                <img src={`https://via.placeholder.com/300x150?text=Voce+Aqui+;)`} alt=""/>
-              </div>
+            {parceiros.map(parceiro => (
+              <a href={parceiro.url} target="_blank" rel="noref" className="column is-3 has-text-centered">
+                <div style={{ height: 100, margin: '40px 20px', background: `url(${parceiro.image}) center center / contain no-repeat`  }}></div>
+              </a>
             ))}
           </div>
         </div>
